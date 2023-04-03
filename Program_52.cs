edfe0390e37248bb4ddcf4dcec3d981ba47be1fb
NGreben <96392178+NGreben@users.noreply.github.com>
2023-04-03 19:52:53 +0300

    Add files via upload

---
+++ Program_52.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+int[,] array = CreateArray(3, 4, -2, 2);
+PrintArray(array);
+
+double sum;
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.WriteLine($"Средднее арифметическое {j}-столбца: {Math.Round(sum / array.GetLength(0), 1)}");
+}
+
+int[,] CreateArray(int row, int column, int min, int max)
+{
+    int[,] array = new int[row, column];
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(min, max);
+    return array;
+}
+
+void PrintArray(int[,] array, string title = " ")
+{
+    if (title != " ")
+        Console.Write($"{title}\n");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
